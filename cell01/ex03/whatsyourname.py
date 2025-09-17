c04cc2ed7eab6cbf658e2d380739ff1c690493ea
67125041-pixel <67125041@kmitl.ac.th>
2025-09-17 12:10:52 +0700

    whatsyourname

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,4 @@
-first_name = "akkarin"
-last_name = "chanma"
-print(first_name + " " + last_name)
+first_name = input("Hey, what's your first name? : ")
+last_name = input("And your last name? : ")
+
+print(f"Well, pleased to meet you, {first_name} {last_name}.")
