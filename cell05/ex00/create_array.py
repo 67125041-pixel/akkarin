62248a5325a58ceed527e2c52e605c49eae6032d
67125041-pixel <67125041@kmitl.ac.th>
2025-09-18 09:38:55 +0700

    s

--- cell05/ex00/create_array.py
+++ cell05/ex00/create_array.py
@@ -1,12 +1,7 @@
+
 def main():
-    # รับเลขจากผู้ใช้ (คั่นด้วย space)
     user_input = input("ป้อนตัวเลข (คั่นด้วย space): ")
-
-    # แปลงเป็น list ของ int
     numbers = [int(x) for x in user_input.split()]
-
-    # แสดง array
     print(numbers)
-
 if __name__ == "__main__":
     main()
