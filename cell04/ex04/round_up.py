bae33b77a8e0379a55d9e52144c435119b8b6b32
67125041-pixel <67125041@kmitl.ac.th>
2025-09-18 08:20:33 +0700

    ro

--- cell04/ex04/round_up.py
+++ cell04/ex04/round_up.py
@@ -1,2 +1,14 @@
-num = float(input("Give me a number: "))
-print(round(num))
+import math
+
+def main():
+    num_str = input("Give me a number: ")
+    try:
+        num = float(num_str)
+       
+        rounded = math.ceil(num)
+        print(rounded)
+    except ValueError:
+        print("That's not a valid number.")
+
+if __name__ == "__main__":
+    main()
